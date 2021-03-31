2562b9f548a08412339b939455b21e63e02a81d4
Karl Dreher <karldreher@outlook.com>
2021-03-30 20:34:28 -0700

    add localstorage
    Default options are now set through localstorage.  right now this is only an option for bypasscache.

--- background.js
+++ background.js
@@ -1,16 +1,31 @@
+let bypassCacheDefaultSetting = { 'bypassCacheOption': true };
+
+function setDefaults() {
+	chrome.storage.local.set(bypassCacheDefaultSetting);
+	console.log('Default cache bypass setting set to \'%s\'', bypassCacheDefaultSetting['bypassCacheOption'])
+}
 
 function refreshAll() {
-	chrome.tabs.query({
-		currentWindow: true
-	},
-		function (tabs) {
-			for (i of tabs) {
-				chrome.tabs.reload(i.id, { bypassCache: false }, console.log("refreshed tab id:" + i.id))
-			}
+	chrome.storage.local.get(['bypassCacheOption'],
+		function (bypassCacheOption) {
+			chrome.tabs.query({
+				currentWindow: true
+			},
+				function (tabs) {
+					for (i of tabs) {
+						chrome.tabs.reload(i.id, { bypassCache: bypassCacheOption['bypassCacheOption'] }, console.log("refreshed tab id:" + i.id))
+					}
+				}
+			)
 		}
 	)
 }
 
-chrome.browserAction.onClicked.addListener(refreshAll)
 
-chrome.commands.onCommand.addListener(refreshAll);
+chrome.runtime.onInstalled.addListener(setDefaults);
+
+chrome.browserAction.onClicked.addListener(refreshAll);
+
+chrome.commands.onCommand.addListener(refreshAll)
+
+
