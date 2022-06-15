7f04e5c90cc9c1ecc209b49848c5a3759154759a
Karl Dreher <karldreher@outlook.com>
2022-06-15 08:53:51 -0700

    formatting

--- background.js
+++ background.js
@@ -1,62 +1,61 @@
 function setDefaults() {
-	let bypassCacheDefaultSetting = { 'bypassCache': false };
-	let includeActiveTabDefaultSetting = { 'includeActiveTab': true }
-	let refreshAudibleTabsSetting = { 'refreshAudibleTabs': false }
-	chrome.storage.local.set(bypassCacheDefaultSetting);
-	chrome.storage.local.get(['bypassCache'],
-		function (setting) {
-			console.log('Default cache bypass setting set to \'%s\'', setting.bypassCache)
-		}
-	);
-	chrome.storage.local.set(includeActiveTabDefaultSetting);
-	chrome.storage.local.get(['includeActiveTab'],
-		function (setting) {
-			console.log('Default include active tab setting set to \'%s\'', setting.includeActiveTab)
-		}
-	);
-	chrome.storage.local.set(refreshAudibleTabsSetting);
-	chrome.storage.local.get(['refreshAudibleTabs'],
-		function (setting) {
-			console.log('Default audible tab refresh setting set to \'%s\'', setting.refreshAudibleTabs)
-		}
-	)
+  let bypassCacheDefaultSetting = { bypassCache: false };
+  let includeActiveTabDefaultSetting = { includeActiveTab: true };
+  let refreshAudibleTabsSetting = { refreshAudibleTabs: false };
+  chrome.storage.local.set(bypassCacheDefaultSetting);
+  chrome.storage.local.get(["bypassCache"], function (setting) {
+    console.log(
+      "Default cache bypass setting set to '%s'",
+      setting.bypassCache
+    );
+  });
+  chrome.storage.local.set(includeActiveTabDefaultSetting);
+  chrome.storage.local.get(["includeActiveTab"], function (setting) {
+    console.log(
+      "Default include active tab setting set to '%s'",
+      setting.includeActiveTab
+    );
+  });
+  chrome.storage.local.set(refreshAudibleTabsSetting);
+  chrome.storage.local.get(["refreshAudibleTabs"], function (setting) {
+    console.log(
+      "Default audible tab refresh setting set to '%s'",
+      setting.refreshAudibleTabs
+    );
+  });
 }
 
 function getOptions(callback) {
-	chrome.storage.local.get(null, function (localStorageItems) {
-		callback(localStorageItems)
-	}
-	)
-
+  chrome.storage.local.get(null, function (localStorageItems) {
+    callback(localStorageItems);
+  });
 }
 
 function refreshAll() {
-	getOptions(function (extensionOptions) {
+  getOptions(function (extensionOptions) {
+    let queryoptions = { currentWindow: true };
+    let reloadProperties = { bypassCache: extensionOptions.bypassCache };
 
-		let queryoptions = { currentWindow: true }
-		let reloadProperties = { bypassCache: extensionOptions.bypassCache }
-
-		chrome.tabs.query(queryoptions,
-			function (tabs) {
-				if (extensionOptions.refreshAudibleTabs == false) {
-					tabs = tabs.filter(tab=>tab.audible != true)
-				}
-				if (extensionOptions.includeActiveTab == false) {
-					tabs = tabs.filter(tab=>tab.active != true)
-				}
-				for (i of tabs) {
-					chrome.tabs.reload(i.id, reloadProperties, console.log("refreshed tab id:" + i.id))
-				}
-			}
-		)
-	})
+    chrome.tabs.query(queryoptions, function (tabs) {
+      if (extensionOptions.refreshAudibleTabs == false) {
+        tabs = tabs.filter((tab) => tab.audible != true);
+      }
+      if (extensionOptions.includeActiveTab == false) {
+        tabs = tabs.filter((tab) => tab.active != true);
+      }
+      for (i of tabs) {
+        chrome.tabs.reload(
+          i.id,
+          reloadProperties,
+          console.log("refreshed tab id:" + i.id)
+        );
+      }
+    });
+  });
 }
-
 
 chrome.runtime.onInstalled.addListener(setDefaults);
 
 chrome.action.onClicked.addListener(refreshAll);
 
-chrome.commands.onCommand.addListener(refreshAll)
-
-
+chrome.commands.onCommand.addListener(refreshAll);
--- options.js
+++ options.js
@@ -1,42 +1,62 @@
 function html_val_to_bool(value) {
-    //seems like a bad idea, but <setting> value cannot be bool and would pass "true" to JS as a string.  
-    if (value == "On") { bool = true }
-    else if (value == "Off") { bool = false }
-    return bool
+  //seems like a bad idea, but <setting> value cannot be bool and would pass "true" to JS as a string.
+  if (value == "On") {
+    bool = true;
+  } else if (value == "Off") {
+    bool = false;
+  }
+  return bool;
 }
 
 function bool_to_html(bool) {
-    //see comments for html_val_to_bool.
-    if (bool == true) { value = "On" }
-    else if (bool == false) { value = "Off" }
-    return value
+  //see comments for html_val_to_bool.
+  if (bool == true) {
+    value = "On";
+  } else if (bool == false) {
+    value = "Off";
+  }
+  return value;
 }
 
-
 function save_options() {
-    var bypassCacheSetting = html_val_to_bool(document.getElementById('bypasscache').value);
-    var includeActiveTabSetting = html_val_to_bool(document.getElementById('includeActiveTab').value);
-    var refreshAudibleTabsSetting = html_val_to_bool(document.getElementById('audible').value);
-    chrome.storage.local.set({
-        bypassCache: bypassCacheSetting,
-        includeActiveTab: includeActiveTabSetting,
-        refreshAudibleTabs: refreshAudibleTabsSetting
-    }, function () {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 1500);
-    });
+  var bypassCacheSetting = html_val_to_bool(
+    document.getElementById("bypasscache").value
+  );
+  var includeActiveTabSetting = html_val_to_bool(
+    document.getElementById("includeActiveTab").value
+  );
+  var refreshAudibleTabsSetting = html_val_to_bool(
+    document.getElementById("audible").value
+  );
+  chrome.storage.local.set(
+    {
+      bypassCache: bypassCacheSetting,
+      includeActiveTab: includeActiveTabSetting,
+      refreshAudibleTabs: refreshAudibleTabsSetting,
+    },
+    function () {
+      // Update status to let user know options were saved.
+      var status = document.getElementById("status");
+      status.textContent = "Options saved.";
+      setTimeout(function () {
+        status.textContent = "";
+      }, 1500);
+    }
+  );
 }
 
 function restore_options() {
-    chrome.storage.local.get(null, function (items) {
-        document.getElementById('bypasscache').value = bool_to_html(items.bypassCache);
-        document.getElementById('includeActiveTab').value = bool_to_html(items.includeActiveTab);
-        document.getElementById('audible').value = bool_to_html(items.refreshAudibleTabs);
-    });
+  chrome.storage.local.get(null, function (items) {
+    document.getElementById("bypasscache").value = bool_to_html(
+      items.bypassCache
+    );
+    document.getElementById("includeActiveTab").value = bool_to_html(
+      items.includeActiveTab
+    );
+    document.getElementById("audible").value = bool_to_html(
+      items.refreshAudibleTabs
+    );
+  });
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
