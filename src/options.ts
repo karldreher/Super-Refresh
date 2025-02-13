367a232cd6a39faddc32f3309dfee61a5fd6c5b9
Karl Dreher <karldreher@outlook.com>
2025-02-12 19:47:59 -0800

    build: move options to ts

--- src/options.ts
+++ src/options.ts
@@ -1,28 +1,31 @@
-function html_val_to_bool(value) {
+function html_val_to_bool(value: string): boolean {
   //seems like a bad idea, but <setting> value cannot be bool and would pass "true" to JS as a string.
   if (value == 'On') {
-    bool = true
+    return true
   } else if (value == 'Off') {
-    bool = false
+    return false
   }
-  return bool
+  // Absolutely no reason we should ever get here. 
+  return false
 }
 
-function bool_to_html(bool) {
+function bool_to_html(bool: boolean): string {
   //see comments for html_val_to_bool.
   if (bool == true) {
     return 'On'
   } else if (bool == false) {
     return 'Off'
   }
+  // Absolutely no reason we should ever get here.
+  return 'Off'
 }
 
 function save_options() {
   var bypassCacheSetting = html_val_to_bool(
-    document.getElementById('bypasscache').value
+    document.getElementById('bypasscache')!.textContent!
   )
   var refreshAudibleTabsSetting = html_val_to_bool(
-    document.getElementById('audible').value
+    document.getElementById('audible')!.textContent!
   )
   chrome.storage.local.set(
     {
@@ -31,7 +34,7 @@ function save_options() {
     },
     function () {
       // Update status to let user know options were saved.
-      var status = document.getElementById('status')
+      let status = document.getElementById('status')!
       status.textContent = 'Options saved.'
       setTimeout(function () {
         status.textContent = ''
@@ -42,13 +45,13 @@ function save_options() {
 
 function restore_options() {
   chrome.storage.local.get(null, function (items) {
-    document.getElementById('bypasscache').value = bool_to_html(
+    document.getElementById('bypasscache')!.textContent = bool_to_html(
       items.bypassCache
     )
-    document.getElementById('audible').value = bool_to_html(
+    document.getElementById('audible')!.textContent = bool_to_html(
       items.refreshAudibleTabs
     )
   })
 }
 document.addEventListener('DOMContentLoaded', restore_options)
-document.getElementById('save').addEventListener('click', save_options)
+document.getElementById('save')!.addEventListener('click', save_options)
